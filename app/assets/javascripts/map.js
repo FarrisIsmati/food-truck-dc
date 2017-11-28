adb09ed6f1b27d72d197f3165c5ae1b47dc1b9a7
Farris <farris.ismati@gmail.com>
2017-11-28 15:50:36 -0500

    map with punchbox

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,51 @@
+class Company { // The name of this class doesn't matter, nor does the filepath
+  constructor() {
+    // Code related to this class' setup.  Not required for Punchbox to run
+  }
+  controller() {
+    // Code within here will run on every action of the controller
+  }
+  index() {
+    let mapData = JSON.parse($('#map_index_data').attr('data'))[0]
+    let handler = Gmaps.build('Google');
+    handler.buildMap({ provider: {
+      center: {lat: 38.9072, lng: -77.0369},
+      zoom: 12,
+      minZoom: 12
+    }, internal: {id: 'map'}}, function(){
+      let markers = handler.addMarkers({
+        "lat": 38.9072,
+        "lng": -77.0369,
+      })
+    })
+  }
+
+  show() {
+    let mapData = JSON.parse($('#map_show_data').attr('data'))[0]
+    let handler = Gmaps.build('Google');
+    handler.buildMap({ provider: {
+      center: {lat: 38.9072, lng: -77.0369},
+      zoom: 12,
+      minZoom: 12,
+      maxZoom: 16
+    }, internal: {id: 'map_show'}}, function(){
+      let markers = handler.addMarkers({
+        "lat": 38.9072,
+        "lng": -77.0369,
+      })
+    })
+  }
+
+  account() {
+    // You aren't constrained to using REST actions either
+    // This works with any action defined in your Rails controller
+  }
+  sharedMethod() {
+    // You can have any other methods in here and reference them from other methods in this class!
+    // Just make sure your method names don't conflict with action names
+  }
+}
+
+
+
+Punchbox.on('Companies', Company);
