b2d83f4b5d36b16ccaacd447fdc2dc42a808be3e
Farris <farris.ismati@gmail.com>
2017-11-30 09:55:33 -0500

    truck clustering

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -8,10 +8,10 @@ class Map {
   }
 
   createMap(data){
+    let clusterMarkers = this.clusterMarkers
     let createMarkers = this.createMarkers
     let handler = this.handler
     let mapData = JSON.parse($(data).attr('data'))
-    console.log(mapData)
     handler.buildMap({ provider: {
       center: {lat: 38.9072, lng: -77.0369},
       zoom: 12,
@@ -23,9 +23,42 @@ class Map {
       fullscreenControl: false,
       streetViewControl: false
     }, internal: {id: this.mapId}}, function(){
-      let markers = handler.addMarkers(mapData)
+      let markers = handler.addMarkers(clusterMarkers(mapData))
       handler.bounds.extendWith(markers)
       handler.fitMapToBounds()
     })
+  }
+
+  clusterMarkers(mapData){
+    console.log(mapData)
+    let returnArray = []
+    let lookUp = {}
+    for (let i = 0; i < mapData.length; i++){
+      let lat = mapData[i].lat
+      let lng = mapData[i].lng
+      let coordKey = lat.toString() + lng.toString()
+      if (lookUp[coordKey]){
+        lookUp[coordKey].push(mapData[i])
+      } else {
+        lookUp[coordKey] = [mapData[i]]
+      }
+    }
+    for (let key in lookUp){
+      if (lookUp[key].length > 1){
+        let companies = `<p>Trucks</p>`
+        for (let i = 0; i < lookUp[key].length; i++){
+          companies += `<a href="">${lookUp[key][i].infowindow}</a>`
+        }
+        returnArray.push({
+          lat: lookUp[key][0].lat,
+          lng: lookUp[key][0].lng,
+          infowindow: companies
+        })
+      } else {
+        returnArray.push(lookUp[key][0])
+      }
+    }
+    console.log(returnArray)
+    return returnArray
   }
 }
